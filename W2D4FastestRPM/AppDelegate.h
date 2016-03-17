026396a1a67ea73401260cb566b3e577dd95225b
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-17 15:19:46 -0400

    Initial commit

---
+++ W2D4FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W2D4FastestRPM
+//
+//  Created by Karlo Pagtakhan on 03/17/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
